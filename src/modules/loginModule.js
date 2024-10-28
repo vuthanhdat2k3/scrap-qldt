0bc2c1048a7e83462fef08f66b0197ddb5316887
vuthanhdat2k3 <vuthanhdat19052k3@gmail.com>
2024-10-28 12:28:57 +0700

    update

--- src/modules/loginModule.js
+++ src/modules/loginModule.js
@@ -1,23 +1,23 @@
-  require('dotenv').config();
-  const { crawlWithRetry, tryWithRetry } = require('./retryModule'); // Import các hàm retry
+require('dotenv').config();
+const { crawlWithRetry, tryWithRetry } = require('./retryModule'); // Import các hàm retry
 
-  // Hàm đăng nhập
-  async function login(page) {
-    // Wait for the username input to appear
-    await page.waitForSelector("input[name='username']", { visible: true });
-    
-    // Now you can safely type into the username input
-    await page.type("input[name='username']", process.env.QLDT_USERNAME); // Use your actual username here
+// Hàm đăng nhập
+async function login(page) {
+  // Wait for the username input to appear
+  await page.waitForSelector("input[name='username']", { visible: true });
   
-    // Similarly for the password
-    await page.waitForSelector("input[name='password']", { visible: true });
-    await page.type("input[name='password']", process.env.QLDT_PASSWORD); // Use your actual password here
-  
-    // Click the login button
-    await page.click("button[type='submit']"); // Adjust the selector if needed
-    
-    // Optionally wait for some navigation or element after logging in
-    await page.waitForNavigation({ waitUntil: 'networkidle2' });
-  }
+  // Now you can safely type into the username input
+  await page.type("input[name='username']", process.env.QLDT_USERNAME); // Use your actual username here
 
-  module.exports = { login };
+  // Similarly for the password
+  await page.waitForSelector("input[name='password']", { visible: true });
+  await page.type("input[name='password']", process.env.QLDT_PASSWORD); // Use your actual password here
+
+  // Click the login button
+  await page.click("button[class='btn btn-primary mb-1 ng-star-inserted']"); // Adjust the selector if needed
+  
+  // Optionally wait for some navigation or element after logging in
+  await page.waitForNavigation({ waitUntil: 'networkidle2' });
+}
+
+module.exports = { login };
