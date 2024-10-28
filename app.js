d1088d31e1498cfbdfb710611d7344f018ab2d4a
vuthanhdat2k3 <vuthanhdat19052k3@gmail.com>
2024-10-28 11:35:11 +0700

    update

--- app.js
+++ app.js
@@ -5,16 +5,29 @@ const PORT = process.env.PORT || 3000;
 const puppeteer = require('puppeteer');
 
 
-app.get("/", async (req, res) => {
-  const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
-  const page = await browser.newPage();
-  await page.goto("https://example.com");
-  const text = await page.$eval("h1", (el) => el.textContent);
-  res.send(text);
+pp.get("/", async (req, res) => {
+  try {
+    const browser = await puppeteer.launch({
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage", // Overcome limited resource problems
+        "--headless", // Ensure Chrome runs in headless mode
+        "--disable-gpu" // Disable GPU hardware acceleration
+      ],
+      headless: true, // Ensure headless mode
+    });
 
-  await browser.close();
+    const page = await browser.newPage();
+    await page.goto("https://example.com");
+    const text = await page.$eval("h1", (el) => el.textContent);
+    res.send(text);
+
+    await browser.close();
+  } catch (error) {
+    console.error("Error launching Puppeteer:", error);
+    res.status(500).send("An error occurred");
+  }
 });
 // app.use("/api", require("./src/routers/index"));
 
