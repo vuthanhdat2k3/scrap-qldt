5d0127adf0521e45b62cc9ddf55f83b445319821
vuthanhdat2k3 <vuthanhdat19052k3@gmail.com>
2024-10-28 19:45:58 +0700

    update

--- src/controllers/xemCTDTController.js
+++ src/controllers/xemCTDTController.js
@@ -1,23 +1,57 @@
 // xemCTDTController.js
-const { crawlXemCTDT } = require('../services/xemCTDTService');
+const { crawlXemCTDT } = require("../services/xemCTDTService");
+const fs = require('fs');
+const path = require('path');
 
 const xemCTDTController = {
   async crawlData(req, res) {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(400).json({ message: "Thiếu tài khoản hoặc mật khẩu" });
+    }
     try {
-      const crawlData = await crawlXemCTDT();
+      const crawlData = await crawlXemCTDT(username, password);
 
       if (crawlData) {
         console.log("Crawl dữ liệu thành công!");
-        res.status(200).json({ message: "Crawl dữ liệu thành công!", data: crawlData });
+        const filePath = path.join("dataCrawl", "grades.json");
+
+        // Đọc dữ liệu từ file grades.json
+        fs.readFile(filePath, "utf8", (err, data) => {
+          if (err) {
+            console.error(err); // In lỗi ra console
+            return res
+              .status(500)
+              .json({ error: "Có lỗi khi đọc file dữ liệu" });
+          }
+
+          // Phân tích dữ liệu JSON và trả về
+          try {
+            const gradesData = JSON.parse(data);
+            res.json(gradesData);
+          } catch (parseError) {
+            res
+              .status(500)
+              .json({ error: "Có lỗi khi phân tích dữ liệu JSON" });
+          }
+        });
+        res
+          .status(200)
+          .json({ message: "Crawl dữ liệu thành công!", data: crawlData });
       } else {
         console.log("Crawl dữ liệu không thành công.");
         res.status(500).json({ message: "Crawl dữ liệu không thành công!" });
       }
     } catch (error) {
       console.error("Có lỗi xảy ra khi crawl dữ liệu:", error);
-      res.status(500).json({ message: "Crawl dữ liệu không thành công!", error: error.message });
+      res
+        .status(500)
+        .json({
+          message: "Crawl dữ liệu không thành công!",
+          error: error.message,
+        });
     }
-  }
+  },
 };
 
 module.exports = xemCTDTController;
--- src/modules/loginModule.js
+++ src/modules/loginModule.js
@@ -1,13 +1,15 @@
 // loginModule.js
-async function login(page) {
+async function login(page, username, password) {
   try {
     // Wait for and enter username
     await page.waitForSelector("input[name='username']", { visible: true, timeout: 60000 });
-    await page.type("input[name='username']", process.env.QLDT_USERNAME);
+    // await page.type("input[name='username']", process.env.QLDT_USERNAME);
+    await page.type("input[name='username']", username);
 
     // Wait for and enter password
     await page.waitForSelector("input[name='password']", { visible: true, timeout: 60000 });
-    await page.type("input[name='password']", process.env.QLDT_PASSWORD);
+    // await page.type("input[name='password']", process.env.QLDT_PASSWORD);
+    await page.type("input[name='password']", password);
 
     // Click the login button
     await page.click("button[class='btn btn-primary mb-1 ng-star-inserted']");
