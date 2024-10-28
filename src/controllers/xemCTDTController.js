6920c44ffd4dcac4ae06e568facfb4c1d65a065a
vuthanhdat2k3 <vuthanhdat19052k3@gmail.com>
2024-10-28 14:46:40 +0700

    update

--- src/controllers/xemCTDTController.js
+++ src/controllers/xemCTDTController.js
@@ -1,11 +1,18 @@
-const {crawlXemCTDT} = require('../services/xemCTDTService'); 
+// xemCTDTController.js
+const { crawlXemCTDT } = require('../services/xemCTDTService');
 
 const xemCTDTController = {
   async crawlData(req, res) {
     try {
-      const browser = await crawlXemCTDT();
-      console.log("Crawl dữ liệu thành công!");
-      res.status(200).json({ message: "Crawl dữ liệu thành công!" });
+      const crawlData = await crawlXemCTDT();
+
+      if (crawlData) {
+        console.log("Crawl dữ liệu thành công!");
+        res.status(200).json({ message: "Crawl dữ liệu thành công!", data: crawlData });
+      } else {
+        console.log("Crawl dữ liệu không thành công.");
+        res.status(500).json({ message: "Crawl dữ liệu không thành công!" });
+      }
     } catch (error) {
       console.error("Có lỗi xảy ra khi crawl dữ liệu:", error);
       res.status(500).json({ message: "Crawl dữ liệu không thành công!", error: error.message });
@@ -14,4 +21,3 @@ const xemCTDTController = {
 };
 
 module.exports = xemCTDTController;
-
