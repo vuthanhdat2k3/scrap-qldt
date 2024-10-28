7828a7ddc1f5f9e5274fff92fcc997722cdb7715
vuthanhdat2k3 <vuthanhdat19052k3@gmail.com>
2024-10-28 10:33:20 +0700

    update

--- app.js
+++ app.js
@@ -4,22 +4,19 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const puppeteer = require('puppeteer');
 
-let browser;
 
-// Hàm khởi động Puppeteer
-const startBrowser = async () => {
-    browser = await puppeteer.launch({
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/google-chrome',
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    });
-};
+app.get("/", async (req, res) => {
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+  const page = await browser.newPage();
+  await page.goto("https://example.com");
+  const text = await page.$eval("h1", (el) => el.textContent);
+  res.send(text);
 
-// Route đơn giản
-app.get('/', (req, res) => {
-    res.send('Hello, Express!');
+  await browser.close();
 });
-
-app.use("/api", require("./src/routers/index"));
+// app.use("/api", require("./src/routers/index"));
 
 // Bắt đầu server
 app.listen(PORT, async () => {
